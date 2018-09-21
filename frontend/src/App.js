18cee1b6cb27ac9fb9848dca31af6b0bfc2a9deb
Jonathan Tang <jonathan.tang@myunidays.com>
2018-09-21 06:46:30 +0100

    Add navbar to App.js and tweak App.css for spacing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">
+                Fluffy Potato
+            </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+        </Navbar>
       </div>
     );
   }
