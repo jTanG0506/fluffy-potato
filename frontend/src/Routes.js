464852247d3b954249563db1aae76ba039bb6bf1
Jonathan Tang <jonathan.tang@myunidays.com>
2018-09-22 06:12:38 +0100

    Add the session to the state

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import AppliedRoute from './components/AppliedRoute';
 import Home from './containers/Home';
 import Login from './containers/Login';
 import NotFound from './containers/NotFound';
 
-export default () =>
+export default ({ childProps }) =>
   <Switch>
-    <Route path="/" exact component={Home} />
-    <Route path="/login" exact component={Login} />
+    <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <Route component={NotFound} />
   </Switch>
--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -28,7 +28,7 @@ export default class Login extends Component {
 
     try {
       await Auth.signIn(this.state.email, this.state.password);
-      alert("Logged In");
+      this.props.userHasAuthenticated(true);
     } catch (e) {
       alert(e.message);
     }
