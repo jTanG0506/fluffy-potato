42163af80917b8a012dedd1f97ac8252704888d6
Jonathan Tang <jonathan.tang@myunidays.com>
2018-09-23 10:27:25 +0100

    Redirect user upon login and logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Auth } from 'aws-amplify';
@@ -37,6 +37,7 @@ class App extends Component {
   handleLogout = async event => {
     await Auth.signOut();
     this.userHasAuthenticated(false);
+    this.props.history.push("/login");
   }
 
   render() {
@@ -78,4 +79,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -29,6 +29,7 @@ export default class Login extends Component {
     try {
       await Auth.signIn(this.state.email, this.state.password);
       this.props.userHasAuthenticated(true);
+      this.props.history.push("/");
     } catch (e) {
       alert(e.message);
     }
