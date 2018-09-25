ce75caa123bf6a09df653ef89124ddd8509e2cfe
Jonathan Tang <jonathan.tang@myunidays.com>
2018-09-25 17:33:43 +0100

    Create sign up form and confirmation code form

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import AppliedRoute from './components/AppliedRoute';
 import Home from './containers/Home';
 import Login from './containers/Login';
+import Signup from "./containers/Signup";
 import NotFound from './containers/NotFound';
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     <Route component={NotFound} />
   </Switch>
