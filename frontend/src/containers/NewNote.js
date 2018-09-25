e4d9ec3018fc40444cadf557c1932b920aac8ac3
Jonathan Tang <jonathan.tang@myunidays.com>
2018-09-25 20:37:13 +0100

    Modify create note to upload attachments first

--- frontend/src/containers/NewNote.js
+++ frontend/src/containers/NewNote.js
@@ -4,6 +4,7 @@ import { API } from 'aws-amplify';
 import LoaderButton from '../components/LoaderButton';
 import config from '../config';
 import './NewNote.css';
+import { s3Upload } from '../libs/awsLib';
 
 export default class NewNote extends Component {
   constructor(props) {
@@ -42,7 +43,10 @@ export default class NewNote extends Component {
     this.setState({ isLoading: true });
 
     try {
+      const attachment = this.file ? await s3Upload(this.file) : null;
+
       await this.createNote({
+        attachment,
         content: this.state.content
       });
       this.props.history.push("/");
