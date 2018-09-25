0ff445d0ca3f9095537103e7c3cf9bc6c6fc5304
Jonathan Tang <jonathan.tang@myunidays.com>
2018-09-25 19:54:31 +0100

    Create add new note form

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,7 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 import AppliedRoute from './components/AppliedRoute';
 import Home from './containers/Home';
 import Login from './containers/Login';
-import Signup from "./containers/Signup";
+import Signup from './containers/Signup';
+import NewNote from './containers/NewNote';
 import NotFound from './containers/NotFound';
 
 export default ({ childProps }) =>
@@ -11,5 +12,6 @@ export default ({ childProps }) =>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
     <Route component={NotFound} />
   </Switch>
