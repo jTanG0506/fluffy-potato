accc5e418ced2683a079642b92e4ed29c2d5ad1d
Jonathan Tang <jonathan.tang@myunidays.com>
2018-09-28 19:17:28 +0100

    Make request and render notes list

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { PageHeader, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { API } from 'aws-amplify';
 import './Home.css';
-import { PageHeader, ListGroup } from '../../node_modules/react-bootstrap';
 
 export default class Home extends Component {
   constructor(props) {
@@ -12,8 +13,51 @@ export default class Home extends Component {
     };
   }
 
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+
+    try {
+      const notes = await this.notes();
+      this.setState({ notes });
+    } catch (e) {
+      alert(e);
+    }
+
+    this.setState({ isLoading: false });
+  }
+
+  notes() {
+    return API.get("fluffy-potato", "/notes");
+  }
+
   renderNotesList(notes) {
-    return null;
+    return [{}].concat(notes).map((note, i) => i !== 0
+      ?
+      <ListGroupItem
+        key={note.noteId}
+        href={`/notes/${note.noteId}`}
+        onClick={this.handleNoteClick}
+        header={note.content.trim().split("\n")[0]}>
+        {"Created: " + new Date(note.createdAt).toLocaleString()}
+      </ListGroupItem>
+      :
+      <ListGroupItem
+        key="new"
+        href="/notes/new"
+        onClick={this.handleNoteClick}
+      >
+        <h4>
+          <b>{"\uFF0B"}</b> Create a new note
+              </h4>
+      </ListGroupItem>
+    );
+  }
+
+  handleNoteClick = event => {
+    event.preventDefault();
+    this.props.history.push(event.currentTarget.getAttribute("href"));
   }
 
   renderLander() {
