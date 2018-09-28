6dfd60990da8f5c3608c9567bb3f592ca398fb72
Jonathan Tang <jonathan.tang@myunidays.com>
2018-09-28 19:43:27 +0100

    Create notes viewing page

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -5,6 +5,7 @@ import Home from './containers/Home';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
 import NewNote from './containers/NewNote';
+import Notes from './containers/Notes';
 import NotFound from './containers/NotFound';
 
 export default ({ childProps }) =>
@@ -13,5 +14,6 @@ export default ({ childProps }) =>
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     <Route component={NotFound} />
   </Switch>
